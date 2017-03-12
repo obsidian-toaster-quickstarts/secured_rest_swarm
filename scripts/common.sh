be40f811fc3b6d231ff9ceab3c08fd46fa9590f7
Scott Stark <starksm64@gmail.com>
2017-03-12 01:57:37 -0800

    Remove sso, update scripts to get urls from oc get routes

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,10 +1,35 @@
 REALM=master
-USER=admin
-PASSWORD=admin
+USER=alice
+PASSWORD=password
 CLIENT_ID=demoapp
-SECRET=cb7a8528-ad53-4b2e-afb8-72e9795c27c8
-SSO_HOST=${1:-https://secure-sso-obsidian.e8ca.engint.openshiftapps.com}
-APP=${2:-http://secured-swarm-rest-obsidian.e8ca.engint.openshiftapps.com}
+SECRET=1daa57a2-b60e-468b-a3ac-25bd2dc2eadc
+#SSO_HOST=${1:-https://secure-sso-obsidian.e8ca.engint.openshiftapps.com}
+APP=$1
+echo "Using APP=${APP}"
+
+declare -a routes
+routes=($(oc get routes))
+count=${#routes[@]}
+
+x=0
+while read -r line
+do
+    #echo "${x}:$line"
+    let x=$x+1
+    routeInfo=($line[@])
+    #echo "  + ${routeInfo[0]}"
+    if [ "${routeInfo[0]}" == "secure-sso" ]
+    then
+      SSO_HOST="https://${routeInfo[1]}"
+      echo "SSH_HOST=${SSO_HOST}"
+    fi
+    if [ "${routeInfo[0]}" == "${APP}" ]
+    then
+      APP_URL="http://${routeInfo[1]}"
+      echo "using APP_URL=${APP_URL}"
+    fi
+done < <(oc get routes)
+
 
 function jsonValue() {
   KEY=$1
